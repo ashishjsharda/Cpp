4f187d88a5e0f324fdc34a03043c51e0e0ef4318
Ashish S <ashishjsharda@gmail.com>
2020-08-31 22:03:17 -0400

    Create pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/**
+ * Pointers part 1
+ * @author ashish
+ */
+
+
+int main() {
+    int i;
+    char c[10];
+    std::cout<<"Address of i is "<<&i<<std::endl;
+    std::cout<<"Address of c is "<<&c;
+}
