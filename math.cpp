f90e65f0a4589392c7086e311091d58ef321506e
Ashish S <ashishjsharda@gmail.com>
2020-11-06 17:56:30 -0500

    Create math.cpp

---
+++ math.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+/**
+ * Using Vectors
+ * @author ashish
+ */
+
+int main() {
+   std::cout<<pow(10,2)<<"\n";
+   std::cout<<ceil(10)<<"\n";
+   std::cout<<cos(10)<<"\n";
+   std::cout<<sin(20)<<"\n";
+   std::cout<<floor(30)<<"\n";
+   return 0;
+}
+
